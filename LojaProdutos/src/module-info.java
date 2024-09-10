a8b353081a17c7942dd9ede2a7bdc0f42262d573
Isanojam127 <161389151+Isanojam127@users.noreply.github.com>
2024-09-09 22:55:29 -0300

    projeto inicial
    tela de login

---
+++ LojaProdutos/src/module-info.java
@@ -0,0 +1,8 @@
+module LojaProdutos {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires java.sql;
+	
+	opens br.com.etec.model to javafx.graphics, javafx.fxml;
+}
